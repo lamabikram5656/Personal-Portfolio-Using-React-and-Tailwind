3ecbc2c462785c2432b3c3ddbd76c6a974f424b3
Nullbyte <bikramlama5656@gmail.com>
2025-09-06 15:04:22 +0545

    Mobile Drop Down

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Home from "./Sections/Home";
 import Navbar from "./Ui/Navbar";
 
 const App = () => {
   return (
     <div className="text-3xl font-bold underline">
       <Navbar />
+      <Home />
     </div>
   );
 };
--- src/Ui/Navbar.jsx
+++ src/Ui/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "./Logo";
+import { MdMenu } from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -10,29 +11,32 @@ const Navbar = () => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center font-mono h-16">
           <Logo />
+          <div className="text-2xl absolute right-7 z-40 md:hidden cursor-pointer font-mono">
+            <MdMenu/>
+          </div>
           <div className="flex md:flex items-center space-x-8">
             <div className="hidden md:flex items-center space-x-8">
               <a
                 href="#home"
-                className="text-gray-300 text-lg hover:text-xl hover:text-green-400 transition-all"
+                className="text-gray-300 text-lg hover:scale-120 hover:text-green-500 transition-all"
               >
                 Home
               </a>
               <a
                 href="#about"
-                className="text-gray-300 text-lg hover:text-xl hover:text-green-500 transition-all"
+                className="text-gray-300 text-lg hover:scale-120 hover:text-green-500 transition-all"
               >
                 About
               </a>
               <a
                 href="#projects"
-                className="text-gray-300 text-lg hover:text-xl hover:text-green-500 transition-all"
+                className="text-gray-300 text-lg hover:scale-120 hover:text-green-500 transition-all"
               >
                 Projects
               </a>
               <a
                 href="#contacts"
-                className="text-gray-300 text-lg hover:text-xl hover:text-green-500  transition-all"
+                className="text-gray-300 text-lg hover:scale-120 hover:text-green-500  transition-all"
               >
                 Contacts
               </a>
